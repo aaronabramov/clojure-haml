e7c6511704773d9f6bae8363a4a35d81524295ce
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-08-28 21:33:01 -0500

    Parse levels

--- src/haml/core.clj
+++ src/haml/core.clj
@@ -1,8 +1,21 @@
-(ns haml.core)
+(ns haml.core
+  (:require [clojure.string :as string]))
 
 (def string (slurp "./resources/sample.haml"))
 
+(def tab-size 2)
+
+(def previous-block-level (atom nil))
+
+(def current-block-level  (atom nil))
+
+(defn create-node
+  [line]
+  (let
+    [[whitespaces expression] (rest (re-matches #"(\s*)(.*)" line))
+      level (/ (.length whitespaces) tab-size)]
+    (zipmap [:level :expression] [level expression])))
+
 
 (defn -main []
-  (println (clojure.string/split-lines string))
-  )
+  (map create-node (string/split-lines string)))
